4583725dcdc0f724d42920363167a985bf41ff8d
Logan Miner <loganrobertminer@gmail.com>
2019-08-12 22:59:52 -0400

    Redesign to use single page

---
+++ portfolio-update/portfolio.js
@@ -0,0 +1,127 @@
+$(document).ready(function () {
+    const aboutItems = {
+        h1: 'About Me',
+        p: 'Lorem ipsum text asldkjfasd lasdl kfjasldkfja sldkfjas ldfkjas ldjf asldjf alskjdfl asjflaskjdflasjfla sjdflkjasdlfjasldkfjaslkdfjalskdjfklajdflkasjdflkasjdflkjasdf sdlk jfasl djf asldkfj asf lkjasd flkjasd lkjas flkjas dflkj asdflkj asdflk jasdlfk jasdlfk jasdlfk jasdlfkj asdlf jasld fkjas fdlkj'
+    };
+
+    $('#aboutLink').on('click', function () {
+        const div = $('<div>');
+        const h1 = $('<h1>');
+        const p = $('<p>');
+
+        h1.text(aboutItems.h1);
+        p.text(aboutItems.p);
+
+
+        div.addClass(''); ///style your appended content with bootstrap classes here
+        div.append(h1, p);
+
+
+        $('#main-content').html(div);
+    });
+
+
+    const contactItems = {
+        form: [{
+            input: "<input>",
+            type: "text",
+            name: "name",
+            placeholder: 'Name'
+        }, {
+            input: "<input>",
+            type: "text",
+            name: "name",
+            placeholder: 'Email'
+        }, {
+            input: "<input>",
+            type: "text",
+            name: "name",
+            placeholder: 'Message'
+        }, {
+            input: "<input>",
+            type: "submit",
+            name: "name",
+            placeholder: 'Your name'
+        }]
+    };
+
+    $('#contactLink').on('click', function () {
+        const div = $('<div>');
+        const form = $('<form>');
+        const h1 = $();
+
+        for (let i = 0; i < contactItems.form.length - 1; i++) {
+            const input = $(contactItems.form[i].input);
+            input.attr('type', contactItems.form[i].type);
+            input.attr('name', contactItems.form[i].name);
+            input.attr('placeholder', contactItems.form[i].placeholder);
+            input.addClass('.contact-input')
+            form.append(input);
+        }
+
+        const button =
+
+            // form.append(button);
+
+            div.append(form);
+
+        div.addClass('d-flex flex-column'); //style your appended content with bootstrap classes here
+
+        $('#main-content').html(div);
+    });
+
+    const portfolioItems = [{
+        h1: 'Pokemon Node',
+        p: 'Snippet about piece',
+        img: '<img>',
+        src: "http://via.placeholder.com/200x200"
+    }, {
+        h1: 'Trivia Game',
+        p: 'Snippet about piece',
+        img: '<img>',
+        src: "http://via.placeholder.com/200x200"
+    }, {
+        h1: 'Translate-A-Tweet App',
+        p: 'Project 1',
+        img: '<img>',
+        src: "http://via.placeholder.com/200x200"
+    }, {
+        h1: 'Pokemon Node',
+        p: 'Snippet about piece',
+        img: '<img>',
+        src: "http://via.placeholder.com/200x200"
+    }, {
+        h1: 'Pokemon Node',
+        p: 'Snippet about piece',
+        img: '<img>',
+        src: "http://via.placeholder.com/200x200"
+    }, {
+        h1: 'Pokemon Node',
+        p: 'Snippet about piece',
+        img: '<img>',
+        src: "http://via.placeholder.com/200x200"
+    }];
+
+    $('.dropdown-item').on('click', function () {
+        const index = $(this).attr('value');
+
+        const h1 = $('<h1>');
+        h1.text(portfolioItems[index].h1);
+
+        const p = $('<p>');
+        p.text(portfolioItems[index].p);
+
+        const img = $('<img>');
+        img.attr('src', portfolioItems[index].src);
+
+        const div = $('<div>');
+    
+        div.addClass('d-flex flex-column'); //style your appended content with bootstrap classes here
+        div.append(h1,p,img);   
+        
+        $('#main-content').html(div);
+
+    });
+    //going to need keyword this
+
+});
